1e939d2e24052b4a8ffd837c3fbf56c77736646a
mementorc <claude.rc@gmail.com>
2026-01-28 16:39:57 -0600

    (fix) git got vx.xx.xx

--- recipe/fix-deps-edn.ps1
+++ recipe/fix-deps-edn.ps1
@@ -1,6 +1,6 @@
 $c = Get-Content 'deps.edn' -Raw                                                                                                                      
 $repo = "`n :mvn/repos`n {`"local`" {:url `"file:$($env:USERPROFILE.Replace('\','/'))/.m2/repository`"}}`n"                                           
 $c = $c -replace '(\{:paths \["src"\])', "`$1$repo"                                                                                                   
-$c = $c -replace ':git/tag', ':mvn/version'                                                                                                           
+$c = $c -replace ':git/tag v', ':mvn/version'                                                                                                           
 $c = $c -replace ' :git/sha +"[a-f0-9]+"', ''                                                                                                         
 Set-Content 'deps.edn' $c                                                                                                                             
